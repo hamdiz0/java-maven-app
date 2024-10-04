b35159dcc96a6d32b1e2fa497ca4c7f0fe253aa5
hamdiz0 <hamdizaghouan21166@gmail.com>
2024-10-04 19:10:32 +0100

    updated

---
+++ script.groovy
@@ -0,0 +1,23 @@
+def jar_build() {
+    echo "building the application..."
+    sh 'mvn package'
+} 
+
+def image_build() {
+    echo "building the docker image..."
+    withCredentials([usernamePassword(
+        credentialsId: 'docker-hub-repo', 
+        usernameVariable: 'USER'
+        passwordVariable: 'PASSWORD', 
+        )]) {
+        sh "docker build -t hamdiz0/java-maven-app:1.2 && \
+        echo $PASSWORD | docker login -u $USER --password-stdin && \
+        docker push hamdiz0/java-maven-app:1.2"
+    }
+} 
+
+def deployApp() {
+    echo 'deploying the application...'
+} 
+
+return this
